93a118766d34a868a333d32c463358402b0572d3
Faiq Athif Nurrahim Hadiko <100032449+fqathf@users.noreply.github.com>
2025-05-09 14:44:59 +0700

    Commit - ShoppingItem data model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ksp {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
     }
 
     buildTypes {
@@ -49,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.room.runtime)
--- app/src/main/java/com/faiqathifnurrahimhadiko607062330082/assessment2/database/ShoppingItem.kt
+++ app/src/main/java/com/faiqathifnurrahimhadiko607062330082/assessment2/database/ShoppingItem.kt
@@ -3,11 +3,16 @@ package com.faiqathifnurrahimhadiko607062330082.assessment2.database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+enum class Priority {
+    HIGH, MEDIUM, LOW
+}
+
 @Entity(tableName = "shopping_items")
 data class ShoppingItem(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val name: String,
-    val quantity: Int,
-    val priority: String, // "high", "medium", "low"
-    val isChecked: Boolean = false
-)
+    val priority: Priority,
+    val isChecked: Boolean = false,
+    val isArchived: Boolean = false
+) 
